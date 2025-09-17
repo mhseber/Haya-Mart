0efe56e9c9233a81f3dcfcbd288e8c004e6b9eec
mahamudul hasan seber <sebermh@gmail.com>
2025-09-18 01:58:09 +0600

    destructure API response to get single meal object instead of array

--- src/app/meals/[id]/page.jsx
+++ src/app/meals/[id]/page.jsx
@@ -4,31 +4,30 @@ const fetchSingleMeal = async (id) => {
       `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
     );
     const data = await res.json();
-    // setMeals(data?.meals || []);
-    return data.meals;
+    return data.meals || [];
   } catch (error) {
     console.log(error);
     return [];
   }
 };
 
-export async function generateMetadata({ params }) {
-  const [singleMeal] = await fetchSingleMeal(params.id);
-  console.log(singleMeal);
-  return {
-    title: singleMeal.strMeal,
-    description: singleMeal.strInstructions,
-  };
-}
-
 export default async function SingleMealPage({ params }) {
-  const p = await params;
+  const [singleMeal] = await fetchSingleMeal(params.id);
 
-  const singleMeal = await fetchSingleMeal(p?.id);
-
+  if (!singleMeal) {
+    return <p>No Meal Found</p>;
+  }
   return (
-    <div>
-      <p>{JSON.stringify(singleMeal)}</p>
+    <div className="border-2 m-5 p-5">
+      <p className="text-4xl text-sky-500 font-extrabold p-4">
+        {singleMeal?.strMeal}
+      </p>
+      <p className="text-xl text-sky-600 font-bold p-2">
+        {singleMeal?.strInstructions}
+      </p>
+      <p className="text-sky-700 font-light p-4">{singleMeal?.strArea}</p>
+      <p className="text-amber-200">{singleMeal?.strCategory}</p>
+      <p className="text-3xl text-amber-600">{singleMeal?.strMealAlternate}</p>
     </div>
   );
 }
