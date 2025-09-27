94d76850dd0a253c12d195eff02a6d1e5b054a1b
mahamudul hasan seber <sebermh@gmail.com>
2025-09-28 01:13:45 +0600

    Deploy Server Side

--- src/app/dashboard/products/add/components/ProductAddFrom.jsx
+++ src/app/dashboard/products/add/components/ProductAddFrom.jsx
@@ -3,13 +3,15 @@
 import { useRouter } from "next/navigation";
 
 export default function ProductAddFrom() {
+  const { NEXT_PUBLIC_SERVER_ADDRESS } = process.env;
   const router = useRouter();
   const handleSubmit = async (e) => {
     e.preventDefault();
     const form = e.target;
     const productName = form.productName.value;
     const payload = { productName };
-    const res = await fetch("http://localhost:3000/api/items", {
+
+    const res = await fetch(`${NEXT_PUBLIC_SERVER_ADDRESS}/api/items`, {
       method: "POST",
       body: JSON.stringify(payload),
       headers: {
