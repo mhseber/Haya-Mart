a06e27fbb5e50d6bd9cdcb6f1f4c5333a8ab47de
mahamudul hasan seber <sebermh@gmail.com>
2025-09-27 15:30:42 +0600

    deploy project successfully on Vercel

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,15 +1,10 @@
-import { redirect } from "next/navigation";
 import React from "react";
 
-export default async function ProductsPage() {
-  const res = await fetch("http://localhost:3000/api/items", {
-    cache: "force-cache",
-  });
-  const data = await res.json();
+export const dynamic = "force-dynamic";
 
-  if (data.length > 100) {
-    redirect("/");
-  }
+export default async function ProductsPage() {
+  const res = await fetch("http://localhost:3000/api/items");
+  const data = await res.json();
 
   return (
     <ul className="text-center mt-9">
