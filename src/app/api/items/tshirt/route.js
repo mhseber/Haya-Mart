d2334d05696b0e70b612f4038c0c9322d160c8c4
mahamudul hasan seber <sebermh@gmail.com>
2025-10-17 02:16:29 +0600

    Successfully connected MongoDB and implemented working GET API for fetching data

--- src/app/api/items/tshirt/route.js
+++ src/app/api/items/tshirt/route.js
@@ -1,10 +1,20 @@
-// get t-shirt
+import dbConnect from "@/lib/dbConnect";
 
-export async function GET(){
-  const data = {
-    message: "This is a T-Shirt",
-    error: false,
-    status: 200
-  }
-  return Response.json({data})
+// get t-shirt test
+
+// export async function GET(){
+//   const data = {
+//     message: "This is a T-Shirt",
+//     error: false,
+//     status: 200
+//   }
+//   return Response.json({data})
+// }
+
+// get t-shirt
+ export async function GET() {
+ 
+ const data = await dbConnect("t-shirt").find({}).toArray();
+ 
+  return Response.json(data)
 }
