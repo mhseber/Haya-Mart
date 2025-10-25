d1de87f474f64b05365e445ecbe34295d4a7ba17
mahamudul hasan seber <sebermh@gmail.com>
2025-10-26 01:55:18 +0600

    Perfume Section Update

--- src/app/api/items/perfume/route.js
+++ src/app/api/items/perfume/route.js
@@ -1,12 +1,19 @@
-// import dbConnect from "@/lib/dbConnect";
+import dbConnect from "@/lib/dbConnect";
 
 // get perfume test
 
-export async function GET(){
-  const data = {
-    message: "This is a Perfume",
-    error: false,
-    status: 200
-  }
-  return Response.json({data})
+// export async function GET(){
+//   const data = {
+//     message: "This is a Perfume",
+//     error: false,
+//     status: 200
+//   }
+//   return Response.json({data})
+// }
+
+ export async function GET() {
+ 
+ const data = await dbConnect("perfume").find({}).toArray();
+ 
+  return Response.json(data)
 }
