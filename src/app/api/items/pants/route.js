78a16e4cb111afb7e18da72d12ee2a59523019bb
mahamudul hasan seber <sebermh@gmail.com>
2025-10-27 01:59:04 +0600

    Pants Code Update

--- src/app/api/items/pants/route.js
+++ src/app/api/items/pants/route.js
@@ -1,19 +1,21 @@
 
+import dbConnect from "@/lib/dbConnect";
 
 // get Pants test
 
-export async function GET(){
-  const data = {
-    message: "This is a Pants",
-    error: false,
-    status: 200
-  }
-  return Response.json({data})
-}
-
-//  export async function GET() {
- 
-//  const data = await dbConnect("pants").find({}).toArray();
- 
-//   return Response.json(data)
+// export async function GET(){
+//   const data = {
+//     message: "This is a Pants",
+//     error: false,
+//     status: 200
+//   }
+//   return Response.json({data})
 // }
+
+
+ export async function GET() {
+ 
+ const data = await dbConnect("pants").find({}).toArray();
+ 
+  return Response.json(data)
+}
