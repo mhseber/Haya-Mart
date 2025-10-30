ac49642ee94051e5738e2fd4b85174df05e0f096
mahamudul hasan seber <sebermh@gmail.com>
2025-10-30 23:54:58 +0600

    Kifaya Part Done

--- src/app/api/items/kifaya/route.js
+++ src/app/api/items/kifaya/route.js
@@ -1,20 +1,20 @@
-// import dbConnect from "@/lib/dbConnect";
+import dbConnect from "@/lib/dbConnect";
 
 // get Kifaya test
 
-export async function GET(){
-  const data = {
-    message: "This is a Kifaya",
-    error: false,
-    status: 200
-  }
-  return Response.json({data})
-}
+// export async function GET(){
+//   const data = {
+//     message: "This is a Kifaya",
+//     error: false,
+//     status: 200
+//   }
+//   return Response.json({data})
+// }
 
 
-//  export async function GET() {
+ export async function GET() {
  
-//  const data = await dbConnect("kifaya").find({}).toArray();
+ const data = await dbConnect("kifaya").find({}).toArray();
  
-//   return Response.json(data)
-//  }
+  return Response.json(data)
+ }
