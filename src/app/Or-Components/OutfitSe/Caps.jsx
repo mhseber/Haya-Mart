f1814575e772c7ac17ffe84978d967dd85ce1157
mahamudul hasan seber <sebermh@gmail.com>
2025-11-03 02:16:52 +0600

    Caps Modal Section Done

--- src/app/Or-Components/OutfitSe/Caps.jsx
+++ src/app/Or-Components/OutfitSe/Caps.jsx
@@ -7,9 +7,11 @@ import { Pagination } from "swiper/modules";
 import { motion } from "framer-motion";
 import "swiper/css";
 import "swiper/css/pagination";
+import CapsMo from "../ProductModalPage/CapsMo";
 
 const Caps = () => {
   const [items, setItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
   useEffect(() => {
     // fetch("http://localhost:3000/api/items/caps")
     fetch("ProductData/caps.json")
@@ -20,6 +22,16 @@ const Caps = () => {
       .then((data) => setItems(data))
       .catch((err) => console.error("Error fetching T-shirt data:", err));
   }, []);
+
+  // এই ফাংশন দিয়ে modal open করো
+  const handleViewDetails = (item) => {
+    setSelectedItem(item);
+  };
+
+  // modal close করার জন্য
+  const handleCloseModal = () => {
+    setSelectedItem(null);
+  };
 
   return (
     <div className="px-6 py-6">
@@ -79,7 +91,10 @@ const Caps = () => {
                         <button className="btn btn-sm rounded-2xl border-sky-700 mt-2">
                           <FaRegHeart className="text-lg text-sky-500" />
                         </button>
-                        <button className="btn btn-sm rounded-2xl border-sky-700 mt-2">
+                        <button
+                          onClick={() => handleViewDetails(item)}
+                          className="btn btn-sm  rounded-2xl border-sky-700 mt-2"
+                        >
                           <AiFillEye className="text-lg text-sky-500" />
                         </button>
                       </div>
@@ -109,6 +124,11 @@ const Caps = () => {
           ))}
         </Swiper>
       </section>
+
+      {/* ✅ Islamic Modal placed here */}
+      {selectedItem && (
+        <CapsMo item={selectedItem} onClose={handleCloseModal} />
+      )}
 
       {/* Swiper Custom Styling */}
       <style jsx global>{`
