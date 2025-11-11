a62790799dc650e08adedb9fb1f61ba5b4bce813
mahamudul hasan seber <sebermh@gmail.com>
2025-11-12 01:58:26 +0600

    Cart Section update

---
+++ src/app/Or-Components/Cart/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1>Cart </h1>
+    </div>
+  );
+};
+
+export default CartPage;
