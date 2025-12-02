dad0c50ca403df2c7f1f2fbbec414e738f109339
mahamudul hasan seber <sebermh@gmail.com>
2025-12-03 02:25:21 +0600

    Login Successful! Done

--- src/app/AuthUsers/LoginForm.jsx
+++ src/app/AuthUsers/LoginForm.jsx
@@ -5,7 +5,11 @@ import { FcGoogle } from "react-icons/fc";
 import { LuLogIn } from "react-icons/lu";
 import { FaUserCircle } from "react-icons/fa";
 import { useState } from "react";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 import auth from "@/Firebase/firebase.init";
 import Swal from "sweetalert2";
 import { useRouter } from "next/navigation";
@@ -14,7 +18,32 @@ export default function LoginForm() {
   const provider = new GoogleAuthProvider();
   const router = useRouter();
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [loginError, setLoginError] = useState("");
 
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log("Email:", email, "Password:", password);
+
+    // reset status
+    setSuccess(false);
+    setLoginError("");
+
+    // login user
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSuccess(true);
+      })
+      .catch((error) => {
+        console.error("Login error:", error.message);
+        setLoginError(error.message);
+      });
+  };
+
+  // Handle Google Login
   const handleGoogleLogin = () => {
     setLoading(true);
 
@@ -53,26 +82,28 @@ export default function LoginForm() {
       <h2 className="text-2xl font-semibold text-center text-white mb-6">
         Login to Continue
       </h2>
+      <form onSubmit={handleLogin}>
+        {/* Email */}
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="input input-bordered w-full bg-white/20 text-white mb-3"
+        />
 
-      {/* Email */}
-      <input
-        type="email"
-        placeholder="Email"
-        className="input input-bordered w-full bg-white/20 text-white mb-3"
-      />
+        {/* Password */}
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          className="input input-bordered w-full bg-white/20 text-white mb-4"
+        />
 
-      {/* Password */}
-      <input
-        type="password"
-        placeholder="Password"
-        className="input input-bordered w-full bg-white/20 text-white mb-4"
-      />
-
-      {/* Normal Login */}
-      <button className="btn border-2  border-black bg-black text-white font-semibold  w-full mb-3">
-        <LuLogIn className="text-xl" /> Login
-      </button>
-
+        {/* Normal Login */}
+        <button className="btn border-2  border-black bg-black text-white font-semibold  w-full mb-3">
+          <LuLogIn className="text-xl" /> Login
+        </button>
+      </form>
       {/* Google Login */}
       <button
         onClick={handleGoogleLogin}
@@ -82,6 +113,18 @@ export default function LoginForm() {
         <FcGoogle className="text-xl" />
         {loading ? "Loading..." : "Google Login"}
       </button>
+
+      {/* success message */}
+      {success && (
+        <p className="text-green-600">
+          <i>Login Successful!</i>
+        </p>
+      )}
+      {loginError && (
+        <p className="text-red-600">
+          <i>{loginError}</i>
+        </p>
+      )}
     </motion.div>
   );
 }
